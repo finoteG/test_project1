2a9beb9f5ad5f4ecb98dc6d24aa690f528c94f4a
finoteG <finotgirma71@gmail.com>
2023-07-31 20:36:32 -1000

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+import qrcode
+
+app=Flask(__name__, render_template('index.html'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__=='__main__':
+    app.run(debug=True)

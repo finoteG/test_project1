d949d5bc52b0ead0e16c676635cbb7f7befcf6d8
finoteG <finotgirma71@gmail.com>
2023-08-01 11:12:59 -1000

    name change

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app=Flask(__name__, render_template('index.html'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html',"hi" )
 
 
 if __name__=='__main__':
